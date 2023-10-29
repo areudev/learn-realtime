e47b3e93a51269d4dd56c663f8713d325e82e4ce
Chris Lime <chrkalivas@gmail.com>
2023-10-29 13:57:14 +0200

    back init

--- polling/back/server.ts
+++ polling/back/server.ts
@@ -1,8 +1,13 @@
 import express from 'express'
+import morgan from 'morgan'
 
 const app = express()
+app.use(morgan('short'))
+app.use(express.json())
+
 app.use(express.static('front'))
 
 const port = 6969
 app.listen(port)
+
 console.log(`Listening on port ${port}`)
